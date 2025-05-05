929528dcd2d1dd75b6e88687dd922dc34c758ee2
Rodrigo Dantas <rodrigo6031@gmail.com>
2025-05-05 10:01:13 -0300

    teste automatizado do login

--- cypress/e2e/cadastroLoginAdm.cy.js
+++ cypress/e2e/cadastroLoginAdm.cy.js
@@ -10,8 +10,20 @@ Cypress.Commands.add('deleteUsers', () => {
 
 Cypress.Commands.add('cadastroAdministrador', () => {
     cy.visit('/');
+    cy.get('[href="/registrar/"]').click();
+    cy.get('#id_tipo_usuario');
+    cy.get('select[name="tipo_usuario"]').select('Administrador');
+    cy.get('#id_username').type('teste administrador');
+    cy.get('#id_email').type('cypress@teste.com');
+    cy.get('#id_password1').type('12345');
+    cy.get('#id_password2').type('12345');
+    cy.get('button').click();
+});
 
-
+Cypress.Commands.add('loginAdministrador', () => {
+  cy.get('#username').type('teste administrador');
+  cy.get('#password').type('12345');
+  cy.get('button').click();
 });
 
 describe('cadastro como administrador', () => {
@@ -28,6 +40,7 @@ describe('cadastro como administrador', () => {
 
     it('Cadastro', () => {
         cy.cadastroAdministrador();
+        cy.loginAdministrador();
     });
 
 
